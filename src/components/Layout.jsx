d81e8aef08eb0aef8d21f75a55d4dd83625cc124
ASUS <ASUS@LAPTOP-D8LBK4KJ>
2021-08-08 00:45:33 +0700

    header

---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Routes } from '../routes/Routes'
+import { Footer } from './Footer'
+import { Header } from './Header'
+
+
+export const Layout = () => {
+    return (
+        <BrowserRouter>
+            <Route render={props => (
+                <div>
+                    <div className="container">
+                        <Header {...props} />
+                        <div className="main">
+                            <Routes />
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            )} />
+        </BrowserRouter>
+    )
+}
---
+++ src/pages/Catalog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Catalog = () => {
+    return (
+        <div>
+            Catalog
+        </div>
+    )
+}
+
+export default Catalog;
