eb30d44321bcc5d149d1aa5e4132b7e66a0bfe72
ASUS <ASUS@LAPTOP-D8LBK4KJ>
2021-08-09 22:12:23 +0700

    slide

---
+++ src/assets/fake-data/hero-slider.js
@@ -0,0 +1,29 @@
+const img1 = require("../images/slide/1.png").default
+const img2 = require("../images/slide/2.png").default
+const img3 = require("../images/slide/3.png").default
+
+const heroSliderData = [
+    {
+        title: "Đầm xuông",
+        img: img1,
+        color: "orange",
+        description: "Ưu điểm của chiếc đầm này là đơn giản, phù hợp với mọi vóc dáng, mọi phong cách kể cả những cô nàng không theo đuổi sự nữ tính. Đầm suông cũng được yêu thích nhờ lợi thế giúp các cô gái che khuyết điểm cực kỳ hiệu quả, vừa thoải mái vừa lịch sự để bạn có thể tự tin diện trong bất kì tình huống nào, chính vì vậy chúng thường được sử dụng làm đầm công sở hay đầm dự tiệc.",
+        path: "/catalog/jumsuit-to-bong"
+    },
+    {
+        title: "Đầm xòe",
+        img: img2,
+        color: "pink",
+        description: "Tuy là dáng đầm truyền thống đã xuất hiện từ lâu nhưng đầm xòe vẫn luôn được yêu thícch cho đến ngày nay nhờ vẻ thanh lịch, nữ tính nhưng vẫn cực kì năng động, trẻ trung. Đầm xòe cũng có thể diện trong nhiều dịp, chẳng hạn như đi chơi, tiệc tùng hay bất kì sự kiện nào mà nàng muốn trở nên thu hút mà vẫn đủ nhã nhặn.",
+        path: "/catalog/candy-white-dress"
+    },
+    {
+        title: "Đầm ôm",
+        img: img3,
+        color: "orange",
+        description: "Những chiếc đầm dáng ôm khéo léo tôn lên đường cong của người mặc, là lựa chọn tuyệt vời của những cô nàng có thân hình cân đối. Đầm ôm mang lại nét đẹp hiện đại, trẻ trung và đầy tự tin. Bạn có thể chọn một đầm cổ vest dài ngang gối mix cùng giày cao gót và túi xách để đi gặp đối tác, đầm sát nách hay đầm xẻ tà để dạo phố, cafe cuối tuần,...",
+        path: "/catalog/jumsuit-to-bong"
+    },
+]
+
+export default heroSliderData
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import heroSliderData from '../assets/fake-data/hero-slider';
 import Helmet from '../components/Helmet';
+import HeroSlider from '../components/HeroSlider';
 
 export const Home = () => {
     return (
         <Helmet title="Trang chủ">
-            Home
+            <HeroSlider
+                data={heroSliderData}
+                control={true}
+                auto={false}
+                timeOut={4000}
+            />
         </Helmet>
     )
 }
